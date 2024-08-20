75b9c9e7eae2bc75bb38580962aa4dedb146b158
Luiz Miguel <125470344+luizmiguelbarros@users.noreply.github.com>
2024-08-20 14:45:35 -0300

    Add files via upload

--- exercicio1.py
+++ exercicio1.py
@@ -3,3 +3,9 @@ valor2 = int(input("Digite o valor 2: "))
 
 if valor1 > valor2:
     print("O valor 1 é maior")
+
+elif valor2 > valor1:
+    print("O valor 2 é maior")
+
+elif valor1 == valor2:
+    print("Os valores são iguais")
